4d0bc0404ddd9413cc5325310106a022639e23a8
choheejin <hjcho9510@gmail.com>
2023-11-20 14:49:15 +0900

    feat(InputView): 이동 횟수 입력 및 검증

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,10 +7,36 @@ import java.util.List;
 
 public class InputView {
     private static final String FORMAT = ",";
+    private static final String R_NUMBER = "^[\\d]*$";
+
     public List<String> readName() throws IllegalArgumentException {
         String input = Console.readLine().trim();
+        validateNone(input);
         validateFormat(input);
         return Arrays.stream(input.split(FORMAT, -1)).toList();
+    }
+
+    public int readCount() throws IllegalArgumentException {
+        String input = Console.readLine().trim();
+        validateNone(input);
+        validateCount(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateNone(String input) throws IllegalArgumentException {
+        if(input.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateCount(String input) throws IllegalArgumentException {
+        if(!input.matches(R_NUMBER)) {
+            throw new IllegalArgumentException();
+        }
+
+        if(Integer.parseInt(input) <= 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validateFormat(String input) throws IllegalArgumentException{
